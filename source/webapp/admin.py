ba54cef168cb6e7529b41f75d3b31b03021a5739
Victor <vzemskov090583@gmail.com>
2019-06-29 12:03:21 +0600

    Add register models in admin.py

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from webapp.models import UserInfo, Author, Book, BookShelf, Review
 
-# Register your models here.
+
+admin.site.register(UserInfo)
+admin.site.register(Author)
+admin.site.register(Book)
+admin.site.register(BookShelf)
+admin.site.register(Review)
