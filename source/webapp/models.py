a2eaa104e037668b4144c1d0634a49bcda055447
Victor <vzemskov090583@gmail.com>
2019-06-29 10:48:35 +0600

    Add models UserInfo, Author

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserInfo(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='info', verbose_name='Пользователь')
+    name = models.CharField(max_length=255, verbose_name='Ф.И.О.')
+    email = models.EmailField(max_length=50, verbose_name="Почта")
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=255, verbose_name='Ф.И.О.')
+    birth_date = models.DateField(null=True, blank=True, verbose_name='дата рождения')
+    death_date = models.DateField(null=True, blank=True, verbose_name='дата смерти')
+    biography = models.TextField(max_length=2000, null=True, blank=True, verbose_name='Биография')
+    photo = models.ImageField(null=True, blank=True, verbose_name='Фотография')
+
+
+
+
